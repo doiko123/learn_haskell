55efbaf7c3bf4b531b84420fc07241d580c7732e
doiko123 <ayanottyz@gmail.com>
2021-07-02 22:05:40 +0900

    フィボナッチなどの関数を追記

--- baby.hs
+++ baby.hs
@@ -1,8 +1,8 @@
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x > 100
-                        then x
-                        else x * 2
+-- doubleSmallNumber x = if x > z0
+--                         then x
+--                         else x * 2Zaaa
 boomBangs xs = [if x < 10 then "BOOM" else "BANG" | x <- xs, odd x]
 length' xs = sum [1 | _ <- xs]
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
@@ -17,3 +17,62 @@ triples = [(a,b,c) | c <- [1..10], a <- [1..10], b <- [1..10]]
 rightTriangles = [(a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2]
 -- 上記について周囲の長さが24のものだけ出力
 rightTriangles' = [(a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a],a^2 + b^2 == c^2, a + b + c == 24]
+
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN"
+lucky x = "sorry, you're out of luck"
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+-- fib n
+--     |fib 0 = 1
+--     |fib 1 = 1
+--     |fib n = fib (n - 1) + fib (n - 2)
+
+-- main = do
+--   fib 100　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
+
+-- fib :: Int -> Int
+-- fib n max
+--   | n == 0 = do
+--     print 1
+--   | n == 1 = do
+--     print 1
+--   | n <= max = do
+--     print n
+--     return fib (n - 2) + fib (n - 1)
+
+-- fib :: Int -> Int
+-- fib n
+--   | n == 0 = do
+--     print 1
+--   | n == 1 = do
+--     print 1
+
+--   | n <= max = do
+--   | n == fib (n - 2) + fib (n - 1) = do
+--     print n
+
+-- fib :: Integer -> Integer -> IO ()
+-- fib n max
+--     | n == 0 = do
+--         print 1
+--     | n == 1 = do
+--         print 1
+--     | n <= max = do
+--         let one = fib (n - 2)
+--         let two = fib (n - 1)
+--         let sum = one + two
+--         print (sum)
+    -- return =<< fib (n - 2) + fib (n - 1) max
+    -- | otherwise = print 0
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
